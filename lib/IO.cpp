7a8515c25f5a9172578313430c1a9719084e8a44
electriccheffer <electriccheff@gmail.com>
2026-04-11 04:20:47 -0500

    ADDED DirectoryData recursive traversal

--- lib/IO.cpp
+++ lib/IO.cpp
@@ -7,7 +7,7 @@ DirectoryData::DirectoryData(std::filesystem::path& rootPath):rootPath(rootPath)
 std::vector<std::filesystem::directory_entry>& DirectoryData::GetDirectoryData(){
 
 	for(const std::filesystem::directory_entry& entry : 
-			std::filesystem::directory_iterator(this->rootPath)){
+			std::filesystem::recursive_directory_iterator(this->rootPath)){
 		this->directoryData.push_back(entry);
 	}
 	return this->directoryData; 
