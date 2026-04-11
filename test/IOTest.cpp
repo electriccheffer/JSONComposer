edaa90f78a2b1dc5791aba196e733622cd3f3528
electriccheffer <electriccheff@gmail.com>
2026-04-11 04:19:07 -0500

    TEST PASSED DirectoryData filesOnlyCase

--- test/IOTest.cpp
+++ test/IOTest.cpp
@@ -6,10 +6,9 @@
 TEST(TrivialTest,AlwaysPasses){
 
 	EXPECT_TRUE(true); 
-
 }
 
-TEST(IOTest,EmptyDirectory){
+TEST(DirectoryData,EmptyDirectory){
 
 	std::filesystem::path testPath = std::filesystem::path("test")
 				 / "testData" / "directoryTestData" / "emptyDirectory" ; 
@@ -17,4 +16,39 @@ TEST(IOTest,EmptyDirectory){
 	std::vector<std::filesystem::directory_entry> result = directoryData.GetDirectoryData(); 
 	int size = result.size(); 
 	EXPECT_EQ(0,size); 
+}
+
+TEST(DirectoryData,FilesOnlyDirectory){
+
+	std::filesystem::path testPath = std::filesystem::path("test")
+				 / "testData" / "directoryTestData" / "filesOnlyDirectory" ; 
+	DirectoryData directoryData(testPath);
+	std::vector<std::filesystem::directory_entry> result = directoryData.GetDirectoryData(); 
+	std::vector<std::filesystem::path> expectedPaths; 
+	std::filesystem::path firstPath = std::filesystem::path("test") / 
+					"testData" / "directoryTestData" / 
+					"filesOnlyDirectory" / "fileOne.txt" ;
+	std::filesystem::path secondPath = std::filesystem::path("test") 
+					/ "testData" / "directoryTestData" / 
+					"filesOnlyDirectory" / "fileTwo.txt" ;
+	std::filesystem::path thirdPath = std::filesystem::path("test") 
+					/ "testData" / "directoryTestData" / 
+					"filesOnlyDirectory" / "fileThree.txt" ;
+	expectedPaths.push_back(firstPath);
+	expectedPaths.push_back(secondPath); 
+	expectedPaths.push_back(thirdPath); 
+	int count = 0; 
+	int size = result.size(); 
+	int expectedSize = expectedPaths.size(); 
+
+	for(int i = 0 ; i < size ; i++){
+		for(int k = 0 ; k < expectedSize  ; k++){
+			
+			if(result[i].path().string() == expectedPaths[k].string()){
+				count++; 
+			}	
+
+		}
+	}
+	EXPECT_EQ(3,count); 
 }
